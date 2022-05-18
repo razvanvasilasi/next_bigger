a0f0042a59e27e6ef3a5fa3d182b5fd3cf24b67d
razvanvasilasi <102486912+razvanvasilasi@users.noreply.github.com>
2022-05-18 09:06:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def next_bigger(n):
     cap = int(''.join(sorted(str(n), reverse=True)))
     if n == cap:
-        return -1
+        return 0
     f = sorted(str(n))
     for c in range(n+1, cap+1):
         if sorted(str(c)) == f:
